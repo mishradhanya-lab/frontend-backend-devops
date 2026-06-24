c1806d8e24bd3348fc986402e54d44844725d324
Dhanya Mishra <mishradhanya30@gmail.com>
2026-06-24 11:33:32 +0530

    Adding configuration
    Added CI/CD pipeline, terraform, kubernetes files

---
+++ outputs.tf
@@ -0,0 +1,16 @@
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.main.kube_config_raw
+  sensitive = true
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.main.name
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.main.login_server
+}
